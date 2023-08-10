518085be26fe2dd34367b338040044f7b9d77b9f
Sergey <sokolovsergeysse@gmail.com>
2023-08-10 10:46:40 +0300

    class work udemy

--- udemyreact/src/App.js
+++ udemyreact/src/App.js
@@ -1,9 +1,37 @@
+import {Component} from "react";
 
+
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 27,
+            text: '+++'
+        }
+    }
+
+    nextYear = () => {
+        this.setState(state => ({
+                years: state.years + 1
+            }))
+        console.log(this.state)
+    }
+    render() {
+        const {name, surname, link} = this.props;
+        return(
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+                <h1>My name is {name}, surname {surname}, age - {this.state.years}</h1>
+                <a href={link}>My profile</a>
+            </div>
+        )
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <WhoAmI name="Alex" surname="Gopnikov" link="facebook.com"/>
     </div>
   );
 }
